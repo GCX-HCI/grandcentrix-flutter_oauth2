e63d01e10186f0e0d47ac285e2d559fffbdb5ef5
Thomas Weingardt <thomas.weingardt@grandcentrix.net>
2019-12-03 14:43:24 +0100

    make interceptor handler accessible

--- lib/interceptor/interceptor.dart
+++ lib/interceptor/interceptor.dart
@@ -5,9 +5,9 @@ import 'package:flutter_oauth2/oauth2.dart';
 /// [Dio] [Interceptor] which uses OAuth 2.0 to authenticate against a
 /// server before sending a request.
 class OAuth2DioInterceptor implements Interceptor {
-  OAuth2 _handler;
+  OAuth2 handler;
 
-  OAuth2DioInterceptor(this._handler);
+  OAuth2DioInterceptor(this.handler);
 
   @override
   Future onError(DioError error) async => error;
@@ -15,7 +15,7 @@ class OAuth2DioInterceptor implements Interceptor {
   @override
   Future onRequest(RequestOptions options) async {
     try {
-      var token = await _handler.authenticate();
+      var token = await handler.authenticate();
 
       // Add authorization header with bearer token
       options.headers[HeaderType.AUTHORIZATION] =
