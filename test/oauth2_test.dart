7cab0a91f0ea532a2015f3bd62454a95d6e8ede6
Thomas Weingardt <thomas.weingardt@grandcentrix.net>
2019-12-03 17:05:18 +0100

    refactoring

--- test/oauth2_test.dart
+++ test/oauth2_test.dart
@@ -12,26 +12,31 @@ void main() {
   const _ANY_EXPIRES_IN = "123";
 
   Dio _mockClient = MockClient();
-  Credentials _credentials = Credentials("any", "any");
-  Uri _authorizationEndpoint = Uri.https("mock.gcx", "/mockToken");
+  Credentials _anyCredentials = Credentials("any", "any");
+  Uri _anyAuthorizationEndpoint = Uri.https("mock.gcx", "/mockToken");
 
   test('Grant type "client_credentials" returns token', () async {
-    when(_mockClient.post(_authorizationEndpoint.toString()))
+    // Assuming that the client returns a valid token when calling
+    // the authorization endpoint
+    when(_mockClient.post(_anyAuthorizationEndpoint.toString()))
         .thenAnswer((_) => Future.value(Response(data: {
               ResponseDataField.ACCESS_TOKEN: _ANY_ACCESS_TOKEN,
               ResponseDataField.REFRESH_TOKEN: _ANY_REFRESH_TOKEN,
               ResponseDataField.EXPIRES_IN: _ANY_EXPIRES_IN
             })));
 
+    // And the authorization method is set to "client_credentials"
     var config = Config(
-        authorizationEndpoint: _authorizationEndpoint,
+        authorizationEndpoint: _anyAuthorizationEndpoint,
         grantType: GrantType.CLIENT_CREDENTIALS,
-        clientCredentials: _credentials,
+        clientCredentials: _anyCredentials,
         httpClient: _mockClient);
 
+    // If the OAuth2 authentication is called
     OAuth2 handler = OAuth2(config);
-
     var token = await handler.authenticate();
+
+    // Expect the valid token to be returned
     expect(token.accessToken, _ANY_ACCESS_TOKEN);
     expect(token.refreshToken, _ANY_REFRESH_TOKEN);
     expect(token.expiration, _ANY_EXPIRES_IN);
