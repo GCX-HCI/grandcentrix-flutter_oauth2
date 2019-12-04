6ec1a3f96d547ed2a43ef95f20e13c550055eae4
Thomas Weingardt <thomas.weingardt@grandcentrix.net>
2019-12-04 15:46:17 +0100

    add tests

--- test/oauth2_test.dart
+++ test/oauth2_test.dart
@@ -9,21 +9,29 @@ class MockClient extends Mock implements Dio {}
 void main() {
   const _ANY_ACCESS_TOKEN = "anyAccessToken";
   const _ANY_REFRESH_TOKEN = "anyRefreshToken";
-  const _ANY_EXPIRES_IN = "123";
+  const _ANY_EXPIRES_IN = 123;
+  const _ANY_TOKEN_TYPE = AuthorizationType.BEARER;
+  const _ANY_CONTENT_TYPE = "anyContentType";
 
   Dio _mockClient = MockClient();
   Credentials _anyCredentials = Credentials("any", "any");
   Uri _anyAuthorizationEndpoint = Uri.https("mock.gcx", "/mockToken");
+  Headers _anyHeaders = Headers();
+
+  setUp(() {
+    _anyHeaders.add(HeaderType.CONTENT_TYPE, _ANY_CONTENT_TYPE);
+  });
 
   test('Grant type "client_credentials" returns token', () async {
     // Assuming that the client returns a valid token when calling
     // the authorization endpoint
-    when(_mockClient.post(_anyAuthorizationEndpoint.toString()))
+    when(_mockClient.post(_anyAuthorizationEndpoint.toString(),
+            data: anyNamed('data'), options: anyNamed('options')))
         .thenAnswer((_) => Future.value(Response(data: {
               ResponseDataField.ACCESS_TOKEN: _ANY_ACCESS_TOKEN,
-              ResponseDataField.REFRESH_TOKEN: _ANY_REFRESH_TOKEN,
-              ResponseDataField.EXPIRES_IN: _ANY_EXPIRES_IN
-            })));
+              ResponseDataField.EXPIRES_IN: _ANY_EXPIRES_IN,
+              ResponseDataField.TOKEN_TYPE: _ANY_TOKEN_TYPE
+            }, headers: _anyHeaders)));
 
     // And the authorization method is set to "client_credentials"
     var config = Config(
@@ -38,7 +46,37 @@ void main() {
 
     // Expect the valid token to be returned
     expect(token.accessToken, _ANY_ACCESS_TOKEN);
+    expect(token.refreshToken, null);
+    expect(token.expiration, isNot(null));
+  });
+
+  test('Grant type "password" returns token', () async {
+    // Assuming that the client returns a valid token when calling
+    // the authorization endpoint
+    when(_mockClient.post(_anyAuthorizationEndpoint.toString(),
+            data: anyNamed('data'), options: anyNamed('options')))
+        .thenAnswer((_) => Future.value(Response(data: {
+              ResponseDataField.ACCESS_TOKEN: _ANY_ACCESS_TOKEN,
+              ResponseDataField.REFRESH_TOKEN: _ANY_REFRESH_TOKEN,
+              ResponseDataField.EXPIRES_IN: _ANY_EXPIRES_IN,
+              ResponseDataField.TOKEN_TYPE: _ANY_TOKEN_TYPE
+            }, headers: _anyHeaders)));
+
+    // And the authorization method is set to "client_credentials"
+    var config = Config(
+        authorizationEndpoint: _anyAuthorizationEndpoint,
+        grantType: GrantType.PASSWORD,
+        clientCredentials: _anyCredentials,
+        userCredentials: _anyCredentials,
+        httpClient: _mockClient);
+
+    // If the OAuth2 authentication is called
+    OAuth2 handler = OAuth2(config);
+    var token = await handler.authenticate();
+
+    // Expect the valid token to be returned
+    expect(token.accessToken, _ANY_ACCESS_TOKEN);
     expect(token.refreshToken, _ANY_REFRESH_TOKEN);
-    expect(token.expiration, _ANY_EXPIRES_IN);
+    expect(token.expiration, isNot(null));
   });
 }
