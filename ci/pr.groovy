0f5cb6a5307feabaaeecc0256b4f12cbee4965de
Thomas Weingardt <thomas.weingardt@grandcentrix.net>
2019-12-06 15:14:00 +0100

    update docker dart image to 2.6

--- ci/pr.groovy
+++ ci/pr.groovy
@@ -6,7 +6,7 @@ agent("docker") {
         gitCheckout(false)
     }
     stage('test') {
-        docker.image("google/dart:2.3").inside("--env PUB_CACHE=$WORKSPACE/.pub_cache") {
+        docker.image("google/dart:2.6").inside("--env PUB_CACHE=$WORKSPACE/.pub_cache") {
             sh "pub get"
             sh "pub run test"
             sh "pub run tool/reformat.dart"
